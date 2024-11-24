7e50ef2bf7ec02cd8af27e7906c4066426b4a7ea
Mahmoud-Elhoseny <elhoseny916@gmail.com>
2024-11-25 00:27:16 +0200

    feat:

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://back-blog-1.onrender.com',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
